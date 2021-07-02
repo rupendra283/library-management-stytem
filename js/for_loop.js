7040b841e274a5e89191773ea3d3451531a6973c
Rupendra <rupendrasingh406@gmail.com>
2021-07-02 13:27:58 +0530

    learning for loop and about opreater

---
+++ js/for_loop.js
@@ -0,0 +1,13 @@
+//For Loop
+
+// for(let i=0;i<100;i++){
+//     console.log(i);
+// }
+
+//while loop
+
+let j = 0;
+while (j < 100) {
+    console.log(j + 1);
+    J += 1;
+}
---
+++ js/opreater.js
@@ -0,0 +1,59 @@
+//addiction
+let x=5; y=3;
+console.log("x+y=",x+y);
+
+//subtraction
+console.log("x-y=",x-y);
+
+//multiplication
+console.log("x*y=",x*y);
+
+//division
+console.log("x/y=",x/y);
+//remainder
+console.log("x%y=",x%y);
+
+//increment
+console.log("++x=", ++x);
+console.log("x++=",x++);
+console.log("x=",x);
+//decrement
+console.log("--x=",--x);
+console.log("x--=",x--);
+console.log("x=",x);
+//exponantattion
+console.log("x**y=",x**y);
+
+//Comparison opreater in java script 
+//equal opreater
+console.log(2 == 2);
+console.log(2 == "2");
+//not equal opreater
+console.log(3 !=2);
+console.log("hello" != "HELLO");
+//strict equal opreater
+console.log(2 === 2);
+console.log("2" === 2);
+//strict not equal opreater
+console.log( 2 !==2);
+console.log( 2 !=="2");
+
+//logical opreater
+//and
+console.log(true && true);
+console.log(true && false);
+console.log(false && false);
+console.log(false && true);
+//or opreater
+console.log(true || false);
+console.log(true || true);
+console.log(false || true);
+console.log(false || false);
+//not opreater
+console.log(!false);
+//bitwise opreater
+//string opreater
+console.log("hello" + "word");
+let a = "javascript";
+a += 'tutorial';
+console.log(a);
