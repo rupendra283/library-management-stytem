e5b94b33b38cf5e800a6e9390392bfe14a95b0c8
Rupendra <rupendrasingh406@gmail.com>
2021-07-02 16:57:17 +0530

    Add two numbers and math sqrt

---
+++ js/add_two_number.js
@@ -0,0 +1,24 @@
+// const a = 2; b = 3;
+//add two numbers
+// const sum = a + b;
+// console.log("the sum of " +a+ " and sum of " +b + " is " + sum);
+//add two numbers enter by the user
+// const no1= parseInt(prompt("enter the first number"));
+// const no2= parseInt(prompt("enter the second number"));
+//add two numbers
+// const sum = no1+no2;
+// console.log(`The sum of ${no1} and ${no2} is ${sum}`);
+
+//Find square root
+
+const number1 = 2.25;
+
+const number2 = -4;
+const number3 = 'hello';    
+const result1 = Math.sqrt(number1);
+const result2 = Math.sqrt(number2);
+const result3 = Math.sqrt(number3);
+
+console.log(`The square root of ${number1} is ${result1}`);
+console.log(`The square root of ${number2} is ${result2}`);
+console.log(`The square root of ${number3} is ${result3}`);
